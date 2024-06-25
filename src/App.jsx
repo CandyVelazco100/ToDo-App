9509b292455c0ca8051a1d0b2f0c353e942cb9f8
Candy Velazco <a20490742@itmexicali.edu.mx>
2024-06-25 16:07:52 -0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import List from './components/List'
+
+function App() {
+  return(
+    <div className='app'>
+      <List/>
+    </div>
+  )
+}
+export default App
---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const List = () => {
+  return (
+    <section class="container">
+      <div class="heading">
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/756881/laptop.svg" class="heading_img" />
+      </div>
+    </section>
+  )
+}
+
+export default List
