d4d071510b75d1982dd0f9fa4499f8aee87477f0
Candy Velazco <a20490742@itmexicali.edu.mx>
2024-06-25 16:26:42 -0700

    "Updated class attributes to use camelCase (className) instead of lowercase (class) in JSX elements."

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 const List = () => {
   return (
-    <section class="container">
-      <div class="heading">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/756881/laptop.svg" class="heading_img" />
-        <h1 class="heading-title">To-Do List</h1>
+    <section className="container">
+      <div className="heading">
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/756881/laptop.svg" className="heading_img" />
+        <h1 className="heading-title">To-Do List</h1>
       </div>
-      <form class="form">
+      <form className="form">
         <div>
-          <label className="form_label" for="todo">Today I need to </label>
-          <input type="text" class="form_input" id="todo" name="to-do" size="30" autoComplete="off" required/>
-          <button class="button"><span>Submit</span></button>
+          <label className="form_label" htmlFor="todo">Today I need to </label>
+          <input type="text" className="form_input" id="todo" name="to-do" size="30" autoComplete="off" required/>
+          <button className="button"><span>Submit</span></button>
         </div>
       </form>
       <div>
-        <ul class="toDoList"> </ul>
+        <ul className="toDoList"> </ul>
       </div>
     </section>
   )
