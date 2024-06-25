9fe7bff84e13206a956086c7baa64fa85790ef77
Candy Velazco <a20490742@itmexicali.edu.mx>
2024-06-25 16:20:03 -0700

    "Added form and list elements to List component, and imported a new font in index.css"

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,6 +5,17 @@ const List = () => {
     <section class="container">
       <div class="heading">
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/756881/laptop.svg" class="heading_img" />
+        <h1 class="heading-title">To-Do List</h1>
+      </div>
+      <form class="form">
+        <div>
+          <label className="form_label" for="todo">Today I need to </label>
+          <input type="text" class="form_input" id="todo" name="to-do" size="30" autoComplete="off" required/>
+          <button class="button"><span>Submit</span></button>
+        </div>
+      </form>
+      <div>
+        <ul class="toDoList"> </ul>
       </div>
     </section>
   )
